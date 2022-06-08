99178bf5312fe0095f813dfb264738b10be254b2
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-08 17:50:06 +0300

    Assigment complete

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+
+const random = Number.parseInt(Math.random(), 10) + 1;
+
+const num3 = 40;
+const num4 = 9;
+
+const mod = num3 % num4;
+
+const max = Math.max(12, 7, 6, 14, 18,20,9);
